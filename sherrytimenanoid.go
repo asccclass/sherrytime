f4b5cf47d54155cba3264677d54e09b756a5d53d
Chih-Han Liu <andyliu@sinica.edu.tw>
2023-01-11 11:50:08 +0800

    <feature>增加 Nano ID 函數

---
+++ sherrytimenanoid.go
@@ -0,0 +1,16 @@
+package sherrytime
+/*
+   產生Neno ID
+*/
+
+import (
+   "github.com/jaevor/go-nanoid"
+)
+
+func(st *SherryTime) NewNanoID()(string, error) {
+   canonicID, err := nanoid.Standard(21)  // The canonic NanoID is nanoid.Standard(21).
+   if err != nil {
+      return "", err
+   }
+   return canonicID, nil
+}
